9bd0407348ca21f6575338db48f4b7aecd7c4f80
Sakura Blossom <sakurablossom@Sakuras-MacBook-Pro.local>
2021-03-28 11:15:49 -0700

    I got my toggle, my showing, and my closing of my menu bar all set and working swell.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,15 @@
 // MENU TOGGLE
+const navMenu = document.getElementById('nav-menu'),
+    toggleMenu = document.getElementById('nav-toggle'),
+    closeMenu = document.getElementById('nav-close')
 // SHOW MENU
+toggleMenu.addEventListener('click', ()=>{
+  navMenu.classList.toggle('show')
+})
 //CLOSING MENU EVENT LISTENER
+closeMenu.addEventListener('click', ()=>{
+  navMenu.classList.remove('show')
+})
 // MOUSE OVER IMAGE .E.L.
 // GSAP ANIMATION
 // NAVBAR
